a7468ac21b5481c2d0c0e46605e86be02b19108a
Hallef <hallefbruno@hotmail.com>
2021-08-21 00:45:18 -0300

    Alteração no gif

--- src/main/resources/static/alldrink-js/fornecedor/Novo.js
+++ src/main/resources/static/alldrink-js/fornecedor/Novo.js
@@ -11,6 +11,7 @@ $(function (){
             if(cpfCnpj.length === 14 || cpfCnpj.length === 18) {
             } else {
                 new StoreDrink.Mensagem().show("warning","CPF/CNPJ inválido!");
+                $("#divLoading").removeClass("loading");
                 return false;
             }
         }
@@ -18,10 +19,10 @@ $(function (){
         if(telefone) {
             if (telefone.length < 14) {
                 new StoreDrink.Mensagem().show("warning","Telefone inválido!");
+                $("#divLoading").removeClass("loading");
                 return false;
             }
         }
-        $("#divLoading").addClass("loading");
         return true;
     });
 });
