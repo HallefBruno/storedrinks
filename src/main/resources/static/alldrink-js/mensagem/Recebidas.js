e39453fac44ba8ab01a3d9b7771858c9f466ce81
hallefbruno <brunohallef@gmail.com>
2022-03-23 09:45:33 -0300

    Trabalhando na listagem de mensagens

--- src/main/resources/static/alldrink-js/mensagem/Recebidas.js
+++ src/main/resources/static/alldrink-js/mensagem/Recebidas.js
@@ -1,23 +1,30 @@
+/* global moment */
+
 $(function () {
   
   var context = $("#context").val();
   
+  var parametros = {
+    columns: [
+      {data: "id", orderable: false, className: 'text-center'},
+      {data: "destinatario", orderable: false},
+      {data: "mensagem", orderable: false},
+      {data: "lida", orderable: false, className: 'text-center'},
+      {data: "dataHoraMensagemRecebida", orderable: false, className: 'text-center',
+        render: function (data,type,row,meta) {
+          return moment(data).format('dd/MM/yyyy, h:mm:ss a');
+        }
+      }
+    ],
+    btnActions: false
+  };
+
+  setDefaultsDataTable(parametros);
+  
   $('#mensagens').DataTable({
-    processing: true,
-    serverSide: true,
-    language: {
-      url: context+"vendor/internationalisation/pt_br.json"
-    },
     ajax: {
       url: context + "mensagens/pesquisar/".concat($('#lidas').is(':checked'))
-    },
-    columns: [
-      {data: "id"},
-      {data: "destinatario"},
-      {data: "mensagem"},
-      {data: "lida"},
-      {data: "dataHoraMensagemRecebida"}
-    ]
+    }
   });
   
 });
---
+++ src/main/resources/static/alldrink-js/util/Utils.js
@@ -0,0 +1,41 @@
+function setDefaultsDataTable(parametros) {
+  
+  if(parametros.btnActions)  {
+    var buttons =
+    {
+      data: "id", title: "Ações", wrap: true,
+      render: function (id) {
+        var stringButtons = "<button class='btn-detalhe btn btn-outline-info btn-sm' data-detalhe='" + id + "' type='button' title='Detalhes' ><i class='fa fa-info-circle'></i></button>";
+        stringButtons += "<button class='btn-excluir btn btn-outline-danger btn-sm mr-1 ml-1 mt-1 mb-1' data-excluir='" + id + "' type='button' title='Excluir'><i class='fa fa-trash-o'></i></button>";
+        stringButtons += "<button class='btn-editar btn btn-outline-edit btn-sm' data-editar='" + id + "' type='button' title='Editar'><i class='fa fa-pencil'></i></button>";
+        return stringButtons;
+      }
+    };
+    parametros.columns.push(buttons);
+  }
+
+  $.extend(true, $.fn.dataTable.defaults, {
+    searching: false,
+    scrollY: "400px",
+    scrollCollapse: true,
+    serverSide: true,
+    processing: false,
+    destroy: true,
+    pageLength: 10,
+    info: true,
+    lengthChange: false,
+    language: {
+      url: $("#context").val()+"vendor/internationalisation/pt_br.json"
+    },
+    columnDefs: parametros.columnDefs,
+    columns: parametros.columns
+  });
+}
+
+function mascaraStringTel(numero) {
+  if (numero.length === 10) {
+    return numero.replace(/(\d{2})(\d{4})(\d{4})/, '($1) $2-$3');
+  } else {
+    return numero.replace(/(\d{2})(\d{5})(\d{4})/, '($1) $2-$3');
+  }
+}
