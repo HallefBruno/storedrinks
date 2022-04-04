6f278e3d19a582f1e600a2d3144e90b92a94ca05
hallefbruno <brunohallef@gmail.com>
2022-04-04 16:59:30 -0300

    Configurando para war

--- src/main/java/com/store/drinks/StoredrinksApplication.java
+++ src/main/java/com/store/drinks/StoredrinksApplication.java
@@ -2,12 +2,19 @@ package com.store.drinks;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
 @EnableCaching
-public class StoredrinksApplication {
-
+public class StoredrinksApplication extends SpringBootServletInitializer {
+  
+  @Override
+  protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+    return application.sources(StoredrinksApplication.class);
+  }
+  
   public static void main(String[] args) throws Exception {
     SpringApplication.run(StoredrinksApplication.class, args);
   }
