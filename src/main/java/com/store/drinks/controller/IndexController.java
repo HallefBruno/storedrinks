5fd361f78126981eb7849032b85449bd8b6c8c97
hallefbruno <brunohallef@gmail.com>
2022-04-04 22:21:00 -0300

    Ajuste na tela de login Criando nova tela de about

--- src/main/java/com/store/drinks/controller/IndexController.java
+++ src/main/java/com/store/drinks/controller/IndexController.java
@@ -2,13 +2,14 @@ package com.store.drinks.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class IndexController {
-
+  
   @RequestMapping("/")
-  public String index() {
-    return "Dashboard";
+  public ModelAndView index() {
+    return new ModelAndView("Dashboard");
   }
 
 }
--- src/main/java/com/store/drinks/controller/SegurancaController.java
+++ src/main/java/com/store/drinks/controller/SegurancaController.java
@@ -5,11 +5,10 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.server.ResponseStatusException;
 
 @Controller
 public class SegurancaController {
-
+  
   @GetMapping("/login")
   public String login(@AuthenticationPrincipal User user) {
     if (Objects.nonNull(user)) {
