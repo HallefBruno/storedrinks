7191c04f28a65692f2ec406513f8f81fcc9b0c93
sud <brunohallef@gmail.com>
2022-05-13 00:45:54 -0300

    Partindo para tela de movimentação de caixa

--- src/main/resources/static/alldrink-js/menu/menu.js
+++ src/main/resources/static/alldrink-js/menu/menu.js
@@ -15,11 +15,11 @@ $(document).ready(function () {
       $("#divLoading").addClass("loading");
     }
   });
-  
+
   $("li.nav-item a.nav-link").mouseover(function () {
     $(this).prop("style","color: #56008f; background: #f5f5f5; font-weight: 500");
   });
-  
+
   $("li.nav-item a.nav-link").mouseout(function () {
     $(this).attr("style","");
   });
