3d019551d1c2d3eaa49259da0d2c551eb1d30094
sud <brunohallef@gmail.com>
2022-05-15 16:52:48 -0300

    Mudança de layout

--- src/main/resources/static/alldrink-js/movimentacaocaixa/MovimentacaoCaixa.js
+++ src/main/resources/static/alldrink-js/movimentacaocaixa/MovimentacaoCaixa.js
@@ -1,4 +1,32 @@
+const usuarios = [];
+
 $(function () {
   
+  $.get("url",function (response) {
+    usuarios = response;
+  });
+  
+  $("#usuarios").select2({
+    theme: "bootstrap-5",
+    allowClear: true,
+    language: "pt-BR",
+    multiple: false,
+    closeOnSelect: true,
+    data: usuarios,
+    templateResult: templateResultProduto,
+    templateSelection: function (usuario) {
+      if (usuario && usuario.text !== "Usuários") {
+        return $("<span class='badge bg-secondary fw-normal' style='font-size:14px;'>" + usuario.text + "</span>");
+      }
+      return $("<span class=''>" + usuario.text + "</span>");
+    }
+  });
   
 });
+
+function templateResultProduto(usuario) {
+  if (usuario.loading) {
+    return $(`<span class='badge bg-secondary fw-normal' style='font-size:14px;'>${usuario.text}</span>`);
+  }
+  return $("<span class='badge bg-secondary fw-normal' style='font-size:14px;'>" + usuario.text + "</span>");
+}
