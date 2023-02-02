3c383e2dcfa22587258dafcd242fbc98932f5a18
Hallef <hallef.hbls@pf.gov.br>
2023-02-02 01:16:47 -0300

    Consulta

---
+++ src/main/java/com/store/drinks/repository/querys/dashboard/ProdutoMaisVendidosImpl.java
@@ -0,0 +1,127 @@
+package com.store.drinks.repository.querys.dashboard;
+
+import com.store.drinks.repository.filtros.ProdutosMaisVendidosFiltro;
+import com.store.drinks.entidade.ItensVenda;
+import com.store.drinks.entidade.Produto;
+import com.store.drinks.entidade.Usuario;
+import com.store.drinks.entidade.Venda;
+import com.store.drinks.entidade.dto.dashboard.DetalheVendadto;
+import com.store.drinks.entidade.dto.produtosMaisVendidos.ProdutosMaisVendidosdto;
+import com.store.drinks.entidade.wrapper.DataTableWrapper;
+import com.store.drinks.repository.util.JpaUtils;
+import com.store.drinks.service.UsuarioService;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Objects;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+import javax.persistence.Tuple;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Join;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
+import javax.persistence.criteria.Selection;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ProdutoMaisVendidosImpl implements ProdutosMaisVendidosRepositoryCustom {
+  
+  @PersistenceContext
+  private EntityManager entityManager;
+
+  @Autowired
+  private JpaUtils jpaUtils;
+
+  @Override
+  public List<ProdutosMaisVendidosdto> listProdutosMaisVendidos(ProdutosMaisVendidosFiltro filters) {
+    Usuario usuarioLogado = UsuarioService.usuarioLogado();
+    
+    if(Objects.isNull(filters.getUsuarioId())) {
+      filters.setUsuarioId(usuarioLogado.getId());
+    }
+    
+    CriteriaBuilder builder = entityManager.getCriteriaBuilder();
+    CriteriaQuery<Tuple> query = builder.createQuery(Tuple.class);
+    Root<ItensVenda> root = query.from(ItensVenda.class);
+    Join<ItensVenda, Produto> produto = root.join("produto");
+    Join<ItensVenda, Venda> venda = root.join("venda");
+    Join<Venda, Usuario> usuario = venda.join("usuario");
+
+    List<Predicate> predicates = new ArrayList<>();
+    List<Selection<?>> selections = new ArrayList<>();
+
+    selections.add(builder.sum(root.get("quantidade")).alias("quantidade"));
+    selections.add(produto.get("descricaoProduto").alias("descricaoProduto"));
+    query.multiselect(selections);
+    
+    if(Objects.nonNull(filters) && StringUtils.isNotBlank(filters.getDataInicial()) && StringUtils.isNotBlank(filters.getDataFinal()) && Objects.nonNull(filters.getUsuarioId())) {
+      DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd", Locale.getDefault());
+      LocalDate dataInicial = LocalDate.parse(filters.getDataInicial(), formatter);
+      LocalDate dataFinal = LocalDate.parse(filters.getDataFinal(), formatter);
+      predicates.add(builder.between(venda.get("dataVenda"), dataInicial, dataFinal));
+      predicates.add(builder.and(builder.equal(usuario.get("id"), filters.getUsuarioId())));
+    } else if (Objects.nonNull(filters.getUsuarioId())) {
+      predicates.add(builder.and(builder.equal(usuario.get("id"), filters.getUsuarioId())));
+      predicates.add(builder.greaterThanOrEqualTo(venda.get("dataVenda"), LocalDate.now()));
+    } 
+    
+    predicates.add(builder.and(builder.equal(root.get("tenant"),usuarioLogado.getClienteSistema().getTenant())));
+    
+    query.where(predicates.toArray(Predicate[]::new));
+    
+    query.groupBy(produto.get("descricaoProduto"));
+    query.orderBy(builder.desc(builder.sum(root.get("quantidade"))));
+    
+    TypedQuery<Tuple> typedQuery = entityManager.createQuery(query);
+    typedQuery.setMaxResults(50);
+    
+    List<Tuple> listTuple = typedQuery.getResultList();
+    List<ProdutosMaisVendidosdto> list = jpaUtils.parseTuple(listTuple,ProdutosMaisVendidosdto.class);
+    
+    return list;
+  }
+
+  @Override
+  public DataTableWrapper<DetalheVendadto> listVendasTempoReal(Long idUsuario, Integer draw, Integer start, Integer length) {
+    DataTableWrapper<DetalheVendadto> dataTableWrapper = new DataTableWrapper<>();
+    Usuario usuarioLogado = UsuarioService.usuarioLogado();
+    
+    if(Objects.isNull(idUsuario)) {
+      idUsuario = usuarioLogado.getId();
+    }
+    
+    String sqlCount = "  select count (*) ";
+    String sqlSelect = " select venda.id as idvenda, produto.descricao_produto as nomeproduto, itens_venda.quantidade, venda.valor_total_venda as valortotal, usuario.nome as nomevendedor, venda.data_hora_venda as datahoravenda ";
+    String sqlFrom = "   from venda inner join itens_venda on venda.id = itens_venda.venda_id inner join produto on produto.id = itens_venda.produto_id inner join usuario on usuario.id = venda.usuario_id where venda.usuario_id = :idUsuario and venda.data_venda = :dataVenda and venda.tenant = :tenant ";
+    
+    Query query = entityManager.createNativeQuery(sqlSelect+sqlFrom, "DetalheVendadto");
+    Query queryCount = entityManager.createNativeQuery(sqlCount+sqlFrom);
+    
+    query.setParameter("idUsuario", idUsuario);
+    query.setParameter("dataVenda", LocalDate.now());
+    query.setParameter("tenant", usuarioLogado.getClienteSistema().getTenant());
+    query.setFirstResult(start);
+    query.setMaxResults(length);
+    
+    query.getParameters().forEach(q -> queryCount.setParameter(q.getName(), query.getParameterValue(q.getName())));
+    
+    Long count = Long.valueOf(queryCount.getSingleResult().toString());
+    
+    dataTableWrapper.setData(query.getResultList());
+    dataTableWrapper.setRecordsTotal(count);
+    dataTableWrapper.setRecordsFiltered(count);
+    dataTableWrapper.setDraw(draw);
+    dataTableWrapper.setStart(start);
+    
+    return dataTableWrapper;
+  }
+  
+}
---
+++ src/main/java/com/store/drinks/repository/querys/dashboard/ProdutosMaisVendidosRepositoryCustom.java
@@ -0,0 +1,12 @@
+package com.store.drinks.repository.querys.dashboard;
+
+import com.store.drinks.entidade.dto.dashboard.DetalheVendadto;
+import com.store.drinks.repository.filtros.ProdutosMaisVendidosFiltro;
+import com.store.drinks.entidade.dto.produtosMaisVendidos.ProdutosMaisVendidosdto;
+import com.store.drinks.entidade.wrapper.DataTableWrapper;
+import java.util.List;
+
+public interface ProdutosMaisVendidosRepositoryCustom {
+  List<ProdutosMaisVendidosdto> listProdutosMaisVendidos(ProdutosMaisVendidosFiltro filters);
+  DataTableWrapper<DetalheVendadto> listVendasTempoReal(Long idUsuario, Integer draw, Integer start, Integer length);
+}
